eb4f1c91dbb6f93575955e39914475fc7316fc31
colonsemi <colonsemi.solution@gmail.com>
2024-11-01 18:54:13 +0530

    latest code

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
             </div>
             <div className="container-fluid sticky-top p-0">
                 <nav className="navbar navbar-expand-lg navbar-light bg-white ps-3">
-                    <NavLink className="navbar-brand" href="#">
+                    <NavLink className="navbar-brand" to={"/"}>
                         <img src={logo} alt="Terapia" className='w-100' height={'60px'} />
                     </NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
